19a214ea049776975251ccc3ece3f6bbf006b6fc
Anicet Ngrt <anicet.nougaret@zohomail.eu>
2020-11-01 15:34:58 +0100

    refactored platformer controller boilerplate

--- Scenes/Character/Character.gd
+++ Scenes/Character/Character.gd
@@ -1,26 +1,40 @@
 extends KinematicBody2D
 
-export (int) var speed = 1200
-export (int) var jump_speed = -1800
-export (int) var gravity = 4000
+export (int) var speed := 0
+export (int) var jump_speed := 0
+export (int) var gravity := 0
 
-var velocity = Vector2.ZERO
+enum Directions { LEFT = 0, RIGHT }
+
+var velocity := Vector2.ZERO
 
 func get_input():
-	velocity.x = 0
 	if Input.is_action_pressed("walk_right"):
-		velocity.x += speed
-		$Sprite.flip_h = true
-		$Sprite.play("run")
+		on_running_started(Directions.RIGHT)
 	elif Input.is_action_pressed("walk_left"):
-		velocity.x -= speed
-		$Sprite.flip_h = false
-		$Sprite.play("run")
+		on_running_started(Directions.LEFT)
 	else:
-		$Sprite.play("idle")
+		on_running_ended()
 
-func _physics_process(delta):
+
+func _physics_process(delta:float):
 	get_input()
+	update_jump(delta)
+
+
+func on_running_started(direction:int) -> void:
+	$Sprite.play("run")
+	$Sprite.flip_h = direction == Directions.RIGHT
+	if direction == Directions.RIGHT: velocity.x += speed
+	else: velocity.x -= speed
+
+
+func on_running_ended() -> void:
+	$Sprite.play("idle")
+	velocity.x = 0
+
+
+func update_jump(delta:float) -> void:
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	if Input.is_action_just_pressed("jump"):
