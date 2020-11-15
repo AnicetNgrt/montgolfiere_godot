138495387a7ee20aa6c9d1a1aa183cb265c8acb7
Anicet Ngrt <anicet.nougaret@zohomail.eu>
2020-11-15 22:52:51 +0100

    .project change

--- Resources/Artifacts/Artifact.gd
+++ Resources/Artifacts/Artifact.gd
@@ -1,6 +1,11 @@
 class_name Artifact
 extends Resource
 
+enum Directions {
+	NORTH, EAST, SOUTH, WEST, S_WEST, S_EAST, N_WEST, N_EAST
+}
+
 export(Texture) var frame
 export(Texture) var object
 export(Texture) var background
+export(Directions) var direction = Directions.NORTH
--- Scenes/Artifacts/Artifact.gd
+++ Scenes/Artifacts/Artifact.gd
@@ -7,10 +7,38 @@ func set_artifact(val):
 	artifact = val
 	if not is_inside_tree(): yield(self, "ready")
 	if val is Artifact:
-		$Frame.texture = artifact.frame
-		$Painting.texture = artifact.background
-		$Object.texture = artifact.object
+		$FaceA/Frame.texture = artifact.frame
+		$FaceA/Painting.texture = artifact.background
+		$FaceA/Object.texture = artifact.object
+		hide_all_directions()
+		match artifact.direction:
+			Artifact.Directions.NORTH:
+				$FaceB/NORTH.show()
+			Artifact.Directions.SOUTH:
+				$FaceB/SOUTH.show()
+			Artifact.Directions.WEST:
+				$FaceB/WEST.show()
+			Artifact.Directions.EAST:
+				$FaceB/EAST.show()
+			Artifact.Directions.S_EAST:
+				$FaceB/S_EAST.show()
+			Artifact.Directions.S_WEST:
+				$FaceB/S_WEST.show()
+			Artifact.Directions.N_EAST:
+				$FaceB/N_EAST.show()
+			Artifact.Directions.N_WEST:
+				$FaceB/N_WEST.show()
 	else:
-		$Frame.texture = null
-		$Painting.texture = null
-		$Object.texture = null
+		$FaceA/Frame.texture = null
+		$FaceA/Painting.texture = null
+		$FaceA/Object.texture = null
+
+func hide_all_directions():
+	$FaceB/NORTH.hide()
+	$FaceB/SOUTH.hide()
+	$FaceB/WEST.hide()
+	$FaceB/EAST.hide()
+	$FaceB/N_EAST.hide()
+	$FaceB/N_WEST.hide()
+	$FaceB/S_EAST.hide()
+	$FaceB/S_WEST.hide()
