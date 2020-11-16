10ea9c343669cddef37dbd8be29926bc61b07dfb
ThibaultHenrion <60553782+ThibaultHenrion@users.noreply.github.com>
2020-11-16 22:56:45 +0100

    Add wind system

--- Scenes/Balloon/Balloon.gd
+++ Scenes/Balloon/Balloon.gd
@@ -5,12 +5,22 @@ export(int) var MAX_SPEED = 80
 export(int) var FRICTION = 500
 export(float) var MAX_ANGLE_DEGREE = 5
 
+export(float) var WIND_STRENGHT = 2
+export(float) var ARTIFACT_DURATION = 2.0
+
+export(Array, Resource) var cards = []
+var wind:Vector2
+onready var Wind_time:Timer = $Wind_timer
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var input_vector = get_action_balloon()
 	move_balloon(delta, input_vector)
 	balloon_rotation(input_vector)
+	
+	if(Input.is_action_pressed("use_artifact")):
+		start_wind()
 
 func get_action_balloon():
 	var input_vector = Vector2.ZERO
@@ -18,14 +28,14 @@ func get_action_balloon():
 	input_vector.y = Input.get_action_strength("down_balloon") - Input.get_action_strength("up_balloon")
 	return input_vector.normalized()
 
-func move_balloon(delta, input_vector):
+func move_balloon(delta, input_vector:Vector2):
 	if input_vector != Vector2.ZERO:
 		velocity=velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity=velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity + (wind * WIND_STRENGHT))
 
-func balloon_rotation (vector):
+func balloon_rotation (vector:Vector2):
 	if vector.x > 0:
 		self.rotation_degrees = min(self.rotation_degrees + 0.05, MAX_ANGLE_DEGREE)
 	elif vector.x < 0:
@@ -35,3 +45,21 @@ func balloon_rotation (vector):
 			self.rotation_degrees = self.rotation_degrees - min(0.05,abs(self.rotation_degrees))
 		elif self.rotation_degrees < 0:
 			self.rotation_degrees = self.rotation_degrees + min(0.05,abs(self.rotation_degrees))
+
+func start_wind():
+	if(Wind_time.is_stopped() and cards.size() != 0):
+		if(cards[-1].direction == Artifact.Directions.N_EAST or cards[-1].direction == Artifact.Directions.EAST or cards[-1].direction == Artifact.Directions.S_EAST):
+			wind.x = 1
+		if(cards[-1].direction == Artifact.Directions.N_WEST or cards[-1].direction == Artifact.Directions.WEST or cards[-1].direction == Artifact.Directions.S_WEST):
+			wind.x = -1
+		if(cards[-1].direction == Artifact.Directions.S_WEST or cards[-1].direction == Artifact.Directions.SOUTH or cards[-1].direction == Artifact.Directions.S_EAST):
+			wind.y = 1
+		if(cards[-1].direction == Artifact.Directions.N_EAST or cards[-1].direction == Artifact.Directions.NORTH or cards[-1].direction == Artifact.Directions.N_WEST):
+			wind.y = -1
+		wind.normalized()
+		Wind_time.start(ARTIFACT_DURATION)
+		cards.pop_back()
+
+func _on_Timer_timeout():
+	wind = Vector2.ZERO
+	Wind_time.stop()
